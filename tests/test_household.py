cf8ef47ae2804951201cf3370c57e24208e26337
marcel-sk <marcelskura1@gmail.com>
2022-02-23 13:48:28 -0500

    refined prompt tests, next split household into sub modules

--- tests/test_household.py
+++ tests/test_household.py
@@ -10,38 +10,44 @@ class cmdInputTest(unittest.TestCase):
         self.house = Household()                    
         
     '''
-    test prompt_square_footage with in-range, well-formatted input value
-    resultant house.square_footage should then match the input
+    ----------------------Test prompt_for_args()------------------
     '''
-    @patch('eleclib.household.Household.get_input',return_value="10000")
-    def test_prompt_square_footage_1(self, input):          
-        self.house.prompt_square_footage()
-        self.assertEqual(self.house.square_footage,10000)
+
+    '''
+    test prompt_for_args() with a list of user input values. erroneous values followed by realistic inputs.
+    '''
+    @patch('eleclib.household.Household.get_input',side_effect=[
+        "0",          #square footage- incorrect
+        "100000",     #square footage- incorrect
+        "1000sf",     #square footage- extra characters (acceptable)
+        "electrik",   #oven type- incorrect
+        "electric",   #oven type- correct
+        "10",         #oven wattage- incorrect
+        "100000",     #oven wattage- incorrect
+        "5000 watts", #oven wattage- extra characters (acceptable)
+        "gass",       #heating type- incorrect
+        "gas",        #heating type- correct
+        "electrik",   #water heater type- incorrect
+        "electric",   #water heater type- correct
+        "electrik",   #fridge type- incorrect
+        "electric",   #fridge type- correct
+        "10",         #fridge wattage- incorrect
+        "100000",     #fridge wattage- incorrect
+        "250 watts",  #fridge wattage- convert from watts to kwh/yr(acceptable)
+        "",           #light bulb type
+        "",           #roof insulation R-value
+        ""])          #wall insulation R-value
+    def test_prompt_for_args_1(self, input):          
+        self.house.prompt_for_args()
+        print(str(self.house))
         
     '''
-    test prompt_square_footage with out of range value followed by a good input value 
-    resultant house.square_footage should then match the second input.
+    test prompt_for_args() with a list of user input values. Use all defaults.
     '''
-    @patch('eleclib.household.Household.get_input',side_effect=["0","10000"])
-    def test_prompt_square_footage_2(self, input):          
-        self.house.prompt_square_footage()
-        self.assertEqual(self.house.square_footage,10000)
-        
-    '''
-    test prompt_square_footage in-range value but extra spaces added to the input
-    '''
-    @patch('eleclib.household.Household.get_input',return_value=" 10000")
-    def test_prompt_square_footage_3(self, input):          
-        self.house.prompt_square_footage()
-        self.assertEqual(self.house.square_footage,10000)
-        
-    '''
-    test prompt_square_footage in-range value but extra spaces added to the input
-    '''
-    @patch('eleclib.household.Household.get_input',return_value=" 10000 square feet")
-    def test_prompt_square_footage_4(self, input):          
-        self.house.prompt_square_footage()
-        self.assertEqual(self.house.square_footage,10000)
+    @patch('eleclib.household.Household.get_input',side_effect=([""]*10))
+    def test_prompt_for_args_2(self, input):          
+        self.house.prompt_for_args()
+        print(str(self.house))
         
         
 def test_version():
